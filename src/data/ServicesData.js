85360ad35912bd40318eef54c5fb0b8eb2085326
Afzalmax <rafzal0606@gmail.com>
2024-03-31 15:04:05 +0530

    rcnt2

---
+++ src/data/ServicesData.js
@@ -0,0 +1,47 @@
+import {   hot1,hot2,hot3,cake1,cake2,cake3,brownie1,brownie2,brownie3,custom1,custom2,custom3, special1, special2, special3 } from "@media";
+
+export const aboutData = [];
+
+export const serviceData = [
+    {
+        name: "CUSTOMIZED CAKE",
+        image:[ 
+            custom1,
+            custom2,
+            custom3,
+        ],
+      },
+    {
+        name:"HOT PICKS",
+        image:[
+            hot1,
+            hot2,
+            hot3,
+        ],
+    },
+    {
+        name:"CAKES",
+        image:[
+            cake1,
+            cake2,
+            cake3,
+        ],
+    },
+    {
+        name:"BROWNIES",
+        image:[
+            brownie1,
+            brownie2,
+            brownie3,
+        ],
+    },
+    {
+        name:"SPECIAL",
+        image:[
+            special1,
+            special2,
+            special3,
+        ],
+    }
+
+]
